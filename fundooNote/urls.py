fa05dfdd2fdaaba5b50699388b61f2f01e733040
TheJayesh99 <jayeshm.official@gmail.com>
2021-09-03 18:36:24 +0530

    Added : Method to validate the login details

--- fundooNote/urls.py
+++ fundooNote/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('registration.urls')),
+    path('user/',include('login.urls')),
 ]
