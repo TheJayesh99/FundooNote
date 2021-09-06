3aa2b6b85e2b613c81ca3c806457fca2862bbe1b
Jayesh Mali <68943760+TheJayesh99@users.noreply.github.com>
2021-09-06 18:05:06 +0530

    Added : Class based views to perform login and registration

---
+++ user_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from user_api import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path("register/",view=views.Register.as_view(),name="register"),
+    path("login/",view=views.Login.as_view(),name="login")
+]
