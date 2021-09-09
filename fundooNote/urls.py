a6f829d6fa6c938bedbc7592cd86b3a03e6baa5d
Jayesh Mali <68943760+TheJayesh99@users.noreply.github.com>
2021-09-09 16:52:31 +0530

    Added : Method to to perform curd operations on notes

--- fundooNote/urls.py
+++ fundooNote/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('user_api.urls')),
+    path('notes/',include('notes.urls')),
 ]
