216974362cb0eaba36e453967b338ebcddab0b31
TheJayesh99 <jayeshm.official@gmail.com>
2021-09-16 18:14:24 +0530

    Added : Method to get token by using redis

--- notes/utility.py
+++ notes/utility.py
@@ -1,7 +1,9 @@
 import jwt
+import redis
 from django.conf import settings
 from django.http import JsonResponse
 
+redis_instence = redis.Redis(host=settings.REDIS_HOST,port=settings.REDIS_PORT)
 
 class EncodeDecodeToken:
     
@@ -27,7 +29,17 @@ def verify_token(function):
             resp = JsonResponse({'message':'Token Not provided in header'})
             resp.status_code = 400
             return resp
-        decode_token = EncodeDecodeToken.decode_token(request.META.get('HTTP_TOKEN'))
-        request.data["user_id"] = decode_token.get("user_id")
+        request_decode_token = EncodeDecodeToken.decode_token(request.META.get('HTTP_TOKEN'))
+        redis_decode_token = EncodeDecodeToken.decode_token(redis_instence.get("id"))
+        if redis_decode_token.get("user_id") != request_decode_token.get("user_id"): 
+            resp = JsonResponse({
+                'message':'login again',
+                "redis":str(redis_instence.get("id")),
+                "request":request.META.get('HTTP_TOKEN')
+                })
+            resp.status_code = 400
+            return resp
+        request.data["user_id"] = request_decode_token.get("user_id")
         return function(self, request)
     return wrapper
+
